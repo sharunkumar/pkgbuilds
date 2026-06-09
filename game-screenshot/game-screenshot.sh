1faeafd827b022cd1b2646c782a04fba89291699
Sharun <sharun@sharunkumar.com>
2026-06-09 00:52:10 -0700

    feat: prefer class before window title

--- game-screenshot/game-screenshot.sh
+++ game-screenshot/game-screenshot.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-GAME=$(kdotool getactivewindow getwindowname 2>/dev/null)
-[ -z "$GAME" ] && GAME=$(kdotool getactivewindow getwindowclassname 2>/dev/null)
+GAME=$(kdotool getactivewindow getwindowclassname 2>/dev/null)
+[ -z "$GAME" ] && GAME=$(kdotool getactivewindow getwindowname 2>/dev/null)
 [ -z "$GAME" ] && GAME="screenshot"
 GAME=$(echo "$GAME" | tr ' /' '_-')
 mkdir -p "$HOME/Pictures/Screenshots"
